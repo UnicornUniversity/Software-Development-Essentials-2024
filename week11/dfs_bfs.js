57c0d597e538352a55bbad5b1118da83f1f47c96
Garmoska <leonov.eugene@gmail.com>
2024-12-11 11:22:42 +0100

    Create dfs_bfs.js

---
+++ week11/dfs_bfs.js
@@ -0,0 +1,53 @@
+class Node {
+    constructor(value, children) {
+        this.value = value;
+        this.children = [];
+        if (children != null) {
+            this.children = children;
+        }
+    }
+}
+
+const node10 = new Node(10);
+const node9 = new Node(9);
+const node8 = new Node(8);
+const node7 = new Node(7);
+const node6 = new Node(6, [node8]);
+const node5 = new Node(5, [node7]);
+const node4 = new Node(4);
+const node3 = new Node(3, [node9, node10]);
+const node2 = new Node(2, [node4, node5, node6]);
+// root
+const node1 = new Node(1, [node2, node3]);
+
+function dfs(node) {
+    console.log(node.value);
+
+    for (let i = 0; i < node.children.length; i++) {
+        const child = node.children[i];
+        dfs(child);
+    }
+}
+
+// uncomment it if needed
+// dfs(node1);
+
+function bfs(node) {
+    let queue = [node];
+
+    while(queue.length > 0){
+        let nextQueue = [];
+        for (let i = 0; i < queue.length; i++) {
+            const nodeToProcess = queue[i];
+            console.log(nodeToProcess.value);
+
+            for (let j = 0; j < nodeToProcess.children.length; j++) {
+                nextQueue.push(nodeToProcess.children[j]);
+            }
+        }
+
+        queue = nextQueue;
+    }
+}
+
+bfs(node1);
